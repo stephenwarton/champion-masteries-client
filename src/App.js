c13138ba7c3ca62730fa39d4537fdc7e9e2cb7ba
Stephen Warton <stephen.warton@gmail.com>
2018-03-12 15:08:07 -0400

    updated to 8.5.2

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import Navbar from './components/Navbar';
 import ChampionsContainer from './components/ChampionsContainer';
 import Dropdown from './components/Dropdown';
 import * as helpers from './helpers';
-const loading = require('./loading.gif');
 
 //let URL = 'http://localhost:3000/api/v1/mastery';
 let URL = 'https://champion-masteries.herokuapp.com/api/v1/mastery';
-let version = "7.23.1";
+let version = '8.5.2';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -26,15 +25,16 @@ class App extends Component {
       loading: false,
       header: 'Welcome',
       champName: ''
-    }
+    };
   }
 
-  handleSubmit(name){
+  handleSubmit(name) {
     this.setState({
-      loading:true,
+      loading: true,
       header: 'Loading'
     });
-    axios.post(URL, {
+    axios
+      .post(URL, {
         name
       })
       .then(response => {
@@ -45,59 +45,75 @@ class App extends Component {
           fetched: true,
           loading: false,
           header: 'Welcome'
-        })
+        });
       })
       .catch(error => {
         //console.log(error);
       });
   }
 
-  handleSelectChange(option){
-    this.setState({option: option});
+  handleSelectChange(option) {
+    this.setState({ option: option });
   }
 
-  handleChange(e){
-    this.setState({champName: e.target.value});
+  handleChange(e) {
+    this.setState({ champName: e.target.value });
   }
 
   render() {
     return (
       <div>
-        <Navbar handleSubmit={this.handleSubmit}/>
+        <Navbar handleSubmit={this.handleSubmit} />
         <div className="container">
-          {
-            !this.state.fetched || this.state.loading
-            ? <h2>{this.state.header}</h2>
-            : null
-          }
-          {
-            this.state.loading
-            ? <div>
-                <img className="loading-gif" src={loading} alt="loading gif" />
-              </div>
-            : null
-          }
-          {
-            this.state.fetched
-            ? <div>
-                <h2>{this.state.user.name} <img className="profileIcon" src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${this.state.user.icon}.png`} alt={this.state.user.icon}/></h2>
-                <h4>Mastery Level: {helpers.format(this.state.user.masteryLevel)}</h4>
-                <div className="row">
-                  <div className="col-md-4">
-                    <h4>Total Mastery Points: {helpers.format(this.state.user.points)}</h4>
-                  </div>
-                  <div className="col-md-4 form-group">
-                    <input type="text" className="form-control" placeholder="Champion Name" value={this.state.champName} onChange={this.handleChange} />
-                  </div>
-                  <div className="col-md-4 container">
-                    <Dropdown handleSelectChange={this.handleSelectChange}/>
-                  </div>
+          {!this.state.fetched || this.state.loading ? (
+            <h2>{this.state.header}</h2>
+          ) : null}
+          {this.state.loading ? <div>...</div> : null}
+          {this.state.fetched ? (
+            <div>
+              <h2>
+                {this.state.user.name}{' '}
+                <img
+                  className="profileIcon"
+                  src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${
+                    this.state.user.icon
+                  }.png`}
+                  alt={this.state.user.icon}
+                />
+              </h2>
+              <h4>
+                Mastery Level: {helpers.format(this.state.user.masteryLevel)}
+              </h4>
+              <div className="row">
+                <div className="col-md-4">
+                  <h4>
+                    Total Mastery Points:{' '}
+                    {helpers.format(this.state.user.points)}
+                  </h4>
                 </div>
-                <ChampionsContainer masteries={this.state.masteries} champName={this.state.champName} filterOption={this.state.option}/>
+                <div className="col-md-4 form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Champion Name"
+                    value={this.state.champName}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="col-md-4 container">
+                  <Dropdown handleSelectChange={this.handleSelectChange} />
+                </div>
               </div>
-            : null
-            }
-          <div className="footer"><small>Made by Stephen W.</small></div>
+              <ChampionsContainer
+                masteries={this.state.masteries}
+                champName={this.state.champName}
+                filterOption={this.state.option}
+              />
+            </div>
+          ) : null}
+          <div className="footer">
+            <small>Made by Stephen W.</small>
+          </div>
         </div>
       </div>
     );
--- src/components/Champion.js
+++ src/components/Champion.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import * as helpers from '../helpers';
 
-let version = "7.23.1";
+let version = '8.5.2';
 
 class Champion extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(e){
+  handleClick(e) {
     e.preventDefault();
     //console.log(this.props.champion);
   }
@@ -18,29 +18,81 @@ class Champion extends Component {
   render() {
     return (
       <span>
-          <img className="icon" src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${this.props.champion.key}.png`} alt={this.props.champion.key} onClick={this.handleClick} data-toggle="modal" data-target={`#${this.props.champion.key}`}/>
-          <div className="modal fade" id={this.props.champion.key} tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
-            <div className="modal-dialog" role="document">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                </div>
-                <div className="modal-body">
-                  <div className="container-fluid">
-                    <div className="row">
-                      <div className="col-md-6">
-                        <img className="loading" src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${this.props.champion.key}_0.jpg`} alt={this.props.champion.key} />
+        <img
+          className="icon"
+          src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${
+            this.props.champion.key
+          }.png`}
+          alt={this.props.champion.key}
+          onClick={this.handleClick}
+          data-toggle="modal"
+          data-target={`#${this.props.champion.key}`}
+        />
+        <div
+          className="modal fade"
+          id={this.props.champion.key}
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="myModalLabel"
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6">
+                      <img
+                        className="loading"
+                        src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${
+                          this.props.champion.key
+                        }_0.jpg`}
+                        alt={this.props.champion.key}
+                      />
+                    </div>
+                    <div className="col-md-6 modalBody">
+                      <h3 className="modal-title" id="myModalLabel">
+                        {this.props.champion.name}
+                      </h3>
+                      <h4>{this.props.champion.title}</h4>
+                      <div>
+                        Champion Level: {this.props.champion.championLevel}
                       </div>
-                      <div className="col-md-6 modalBody">
-                        <h3 className="modal-title" id="myModalLabel">{this.props.champion.name}</h3>
-                        <h4>{this.props.champion.title}</h4>
-                        <div>Champion Level: {this.props.champion.championLevel}</div>
-                        <div>Champion Points: {helpers.format(this.props.champion.championPoints)}</div>
-                        <div>Points Since Last Level: {helpers.format(this.props.champion.championPointsSinceLastLevel)}</div>
-                        <div>Points Unitl Next Level: {helpers.format(this.props.champion.championPointsUntilNextLevel)}</div>
-                        <div>Mastery Tokens: {this.props.champion.tokensEarned}</div>
-                        <div>Chest Earned: {helpers.chest(this.props.champion.chestGranted)}</div>
-                        <div>Last Time Played: {helpers.formatDate(this.props.champion.lastPlayTime)}</div>
+                      <div>
+                        Champion Points:{' '}
+                        {helpers.format(this.props.champion.championPoints)}
+                      </div>
+                      <div>
+                        Points Since Last Level:{' '}
+                        {helpers.format(
+                          this.props.champion.championPointsSinceLastLevel
+                        )}
+                      </div>
+                      <div>
+                        Points Unitl Next Level:{' '}
+                        {helpers.format(
+                          this.props.champion.championPointsUntilNextLevel
+                        )}
+                      </div>
+                      <div>
+                        Mastery Tokens: {this.props.champion.tokensEarned}
+                      </div>
+                      <div>
+                        Chest Earned:{' '}
+                        {helpers.chest(this.props.champion.chestGranted)}
+                      </div>
+                      <div>
+                        Last Time Played:{' '}
+                        {helpers.formatDate(this.props.champion.lastPlayTime)}
                       </div>
                     </div>
                   </div>
@@ -48,6 +100,7 @@ class Champion extends Component {
               </div>
             </div>
           </div>
+        </div>
       </span>
     );
   }
